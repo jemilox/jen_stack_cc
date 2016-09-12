0fa04f653ffc8a623908c1b4ddf98475949eec79
Lisa Cook <jemilox@gmail.com>
2016-09-12 15:55:22 -0500

    changed server for heroku

--- server/app.js
+++ server/app.js
@@ -3,14 +3,15 @@ var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded( {extended: false});
+var portDecision = process.env.PORT || 3000;
 
 var count = 0;
 
-app.listen('3000', 'localhost', function () {
+app.listen( portDecision, function () {
   console.log("3000 is up!");
 });//end server up
 
-app.get('/', function (req, res) {
+app.get('/', urlencodedParser, function (req, res) {
   console.log('base url hit');
   res.sendFile(path.resolve('public/index.html'));
 });
