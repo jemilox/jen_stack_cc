306319539eae7c49fc1b195f7481367c093870c4
Lisa Cook <jemilox@gmail.com>
2016-09-12 15:52:02 -0500

    final, i think

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,5 +3,65 @@ console.log('sourced');
 $(document).ready(function(){
   console.log('doc ready');
 
+  $('#addJoke').on('click', function () {
+    console.log('in addJoke');
 
-});
+    var author = $('#authorIn').val();
+    var joke = $('#jokeIn').val();
+    var punchLine = $('#punchLineIn').val();
+    console.log(author, joke, punchLine);
+
+    var sentJoke = {authorOut: author, jokeOut: joke, punchLineOut: punchLine};
+    console.log(sentJoke);
+
+    $.ajax({
+      type: 'POST',
+      url: '/jokeSend',
+      data: sentJoke,
+      success: function (data) {
+        console.log('in ajax call');
+
+      }
+
+    });
+
+  });
+
+  $('#tellJoke').on('click', function () {
+    //console.log('in tellJoke');
+    //call for a joke from the server
+    $.ajax({
+      type: 'POST',
+      url: 'getJoke',
+      success: function (data) {
+        //console.log('in ajax getJoke');
+        //console.log(data.whoseJoke);
+        var authorDisplay = data.whoseJoke;
+        var jokeDisplay = data.jokeQuestion;
+        var punchLineDisplay = data.punchLine;
+        //display joke
+        $('.joke').html('<p>'+ jokeDisplay + '</p>');
+        $('.jokeTeller').fadeOut(10, function(){
+          $('.jokeTeller').html('<p>' + punchLineDisplay + '</p><p>-' + authorDisplay + '</p>');
+        });
+        $('.jokeTeller').fadeIn(8000);
+      }//end success
+
+    });//end ajax call for tellJoke
+
+  });//end tellJoke button function
+
+
+});//end doc ready
+
+
+
+
+// var moveToNextStudent = setInterval(function(){
+//   timer ++;
+//   console.log(timer);
+//   if (timer === 10){
+//     timer = 0;
+//   displayStudentsRight();
+//   }
+// }, 1000);
--- server/app.js
+++ server/app.js
@@ -4,6 +4,57 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded( {extended: false});
 
-app.listen('2001', 'localhost', function () {
-  console.log("I'm listening, Dave");
+var count = 0;
+
+app.listen('3000', 'localhost', function () {
+  console.log("3000 is up!");
 });//end server up
+
+app.get('/', function (req, res) {
+  console.log('base url hit');
+  res.sendFile(path.resolve('public/index.html'));
+});
+
+app.use(express.static('public'));
+
+app.post('/jokeSend', urlencodedParser, function ( req, res ) {
+  console.log('in jokeSend post');
+  console.log(req.body);
+  var author = req.body.authorOut;
+  var joke = req.body.jokeOut;
+  var punchLineIn = req.body.punchLineOut;
+
+  jokes.push({whoseJoke: author, jokeQuestion: joke, punchLine: punchLineIn});
+  console.log(jokes);
+
+
+});
+
+// initial jokes provided by the client
+var jokes = [
+  {
+    whoseJoke: "Huck",
+    jokeQuestion: "What's the difference between roast beef and pea soup?",
+    punchLine: "Anyone can roast beef."
+  },
+  {
+    whoseJoke: "Millie",
+    jokeQuestion: "What do you call a pile of cats?",
+    punchLine: "A meowntain!"
+  },
+  {
+    whoseJoke: "dEv",
+    jokeQuestion: "Why should you not play cards in the forest?",
+    punchLine: "Too many Cheetahs"
+  }
+];
+
+app.post('/getJoke', urlencodedParser, function (req, res) {
+  console.log(jokes.length);
+  if (count > jokes.length - 2){
+    count = 0;
+  }else{
+  count ++;}
+  res.send(jokes[count]);
+
+});
